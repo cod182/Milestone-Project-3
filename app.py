69c7edf01620eb6e84cf6c3928b654836d61cf97
Codie Stephens-Evans <codie18@gmail.com>
2021-08-30 10:23:49 +0000

    index page created

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/base")
-def base():
-    return render_template("base.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
