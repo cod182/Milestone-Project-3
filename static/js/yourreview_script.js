068984372743167222c5664895bf6d4b4e3f4a8b
Codie Stephens-Evans <codie18@gmail.com>
2021-09-07 14:21:19 +0000

    styling added to delete button

--- static/js/yourreview_script.js
+++ static/js/yourreview_script.js
@@ -5,7 +5,6 @@ let starRating = document.getElementsByClassName('review-star-rating');
 // When windows loads give the settings icon a blue glow
 window.onload = function(){
   yourreviewicon.classList.add('glow');
-  
 };
 
 // Changes the review rating number to a star
