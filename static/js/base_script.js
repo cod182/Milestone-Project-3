15ab2ee6546eed233602dd0ac92bee2729e19073
Codie Stephens-Evans <codie18@gmail.com>
2021-09-28 21:36:08 +0000

    added option to tun off background

--- static/js/base_script.js
+++ static/js/base_script.js
@@ -1,3 +1,6 @@
+let background = localStorage.getItem('background');
+
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -8,6 +11,22 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 };
 
+// Goes back to previous page
 function goBack() {
   window.history.back();
-}
+};
+
+//Checks if background is on in local storage
+if(background) {
+  if(background === 'on') {
+    addBackground();
+  }
+} else {
+  addBackground();
+};
+
+// Adds the background image and saves on state to local storage
+function addBackground() {
+  document.body.style.background = 'url("https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_400/MTc0MDE0OTk4MzEyMzk2NjY3/asteroids-by-atari-classic-video-games-reviewed.webp")'
+  localStorage.setItem('background', 'on');
+};
--- users/static/js/changepass_script.js
+++ users/static/js/changepass_script.js
@@ -1,6 +1,9 @@
 const settingsSelect = document.getElementById('settings-icon');
 const currentLink = document.getElementById('profile-link');
 const currentNestedLink = document.getElementById('profile-settings-link')
+const backgroundToggle = document.getElementById('background-toggle');
+
+
 
 // When windows loads give the settings icon a blue glow
 window.onload = function () {
@@ -34,4 +37,34 @@ function selectProfile() {
   for (let x = 0; x < profileLink.length; x++) {
     profileLink[x].classList.add('selected');
   }
+}
+
+//Checks if background is on in local storage
+if(background === 'on') {
+  addBackground();
+  backgroundToggle.setAttribute('checked', '');
+};
+
+// Toggle for background image / solid color
+backgroundToggle.addEventListener('click', () => { //listens for dark button clicked
+  background = localStorage.getItem('background');
+  if(background !== 'on') { //if body has a class
+      addBackground(); // add classes
+  } else {    // if body has no class
+      removeBackground(); //remove classed
+  }
+});
+
+// Adds the background image and saves on state to local storage
+function addBackground() {
+  console.log('ON')
+  document.body.style.background = 'url("https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_400/MTc0MDE0OTk4MzEyMzk2NjY3/asteroids-by-atari-classic-video-games-reviewed.webp")'
+  localStorage.setItem('background', 'on');
+}
+
+// sets background to grey and saves off state background to local storage
+// Also sets the back
+function removeBackground() {
+  document.body.style.background = '#444444'
+  localStorage.setItem('background', 'off');
 }
